e115a8afdd9ebc0d50bd3fc5ecc01bd44b244d98
dineshchandrasekaren <dineshchandrasekaren@gmail.com>
2025-09-02 11:28:26 +0530

    center the text for no message component

--- frontend/src/components/chat/no-message.component.jsx
+++ frontend/src/components/chat/no-message.component.jsx
@@ -2,7 +2,7 @@ import Logo from "../logo.component";
 
 const NoMessage = () => {
   return (
-    <div className="w-full h-full flex justify-center items-center flex-col gap-5">
+    <div className="w-full h-full flex justify-center text-center items-center flex-col gap-5">
       {" "}
       <div className="animate-bounce ">
         <Logo size={30} extraClass="shadow" />
@@ -10,7 +10,7 @@ const NoMessage = () => {
       <h2 className="text-2xl font-extrabold text-shadow-xs">
         Welcome to Chatty!
       </h2>
-      <p className="font-medium mb-4 text-base-content/80 text-shadow-xs">
+      <p className="font-medium   text-center mb-4 text-base-content/80 text-shadow-xs">
         Pick a buddy from your list to start chatting.
       </p>
     </div>
